f3175682eb66507088aa5b004e5426f3fad8d1ac
andrey03410 <Andrey.gel001234@mail.ru>
2023-02-21 13:40:40 +0200

    add person form

--- src/pages/AddForm.jsx
+++ src/pages/AddForm.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import './AddForm.css'
 
 const AddForm = () => {
     return (
         <div>
-            Форма
+            <details>
+                <summary>Персона</summary>
+                <div className="forms_wrapper">
+                    <form className="form_wrapper">
+                        Добавить персону
+                        <div>
+                            <p>* ФИО</p>
+                            <input className="form_input"/>
+                        </div>
+                        <div>
+                            <p>* Гражданство</p>
+                            <select name="select" size="3" multiple className="form_input">
+                                <option selected value="s1">СССР</option>
+                                <option value="s2">Германия</option>
+                                <option value="s3">Польша</option>
+                                <option value="s4">Украина</option>
+                            </select>
+                        </div>
+                        <div>
+                            <p>Описание</p>
+                            <textarea className="form_input"/>
+                        </div>
+                        <button>Отправить</button>
+                    </form>
+
+                    <form className="form_wrapper">
+                        Найти персону
+                        <div>
+                            <p>* ФИО</p>
+                            <input className="form_input"/>
+                        </div>
+                        <p>Результаты поиска </p>
+                        <table>
+                            <tr>
+                                <td>&nbsp;</td>
+                                <td>id персоны</td>
+                                <td>ФИО</td>
+                                <td>Описание</td>
+                            </tr>
+                            <tr>
+                                <td>Eating Habits</td>
+                                <td>Eats everyone's leftovers</td>
+                                <td>Nibbles at food</td>
+                                <td>Hearty eater</td>
+                            </tr>
+                        </table>
+                        <button>Отправить</button>
+                    </form>
+                </div>
+            </details>
         </div>
     );
 };
