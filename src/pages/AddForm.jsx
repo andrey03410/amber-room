a9d9e3952f17a2f600d17e2754bfca4986b08bdd
DerkachEkaterina <50298567+DerkachEkaterina@users.noreply.github.com>
2023-02-26 19:46:06 +0200

    add document

--- src/pages/AddForm.jsx
+++ src/pages/AddForm.jsx
@@ -218,10 +218,166 @@ const AddForm = () => {
 {/*Поняла? Обведи ответ: нет, да*/}
             <details className={"detail_wrapper"}>
                 <summary className={"summary_wrapper"}>Документ</summary>
+                <div className={"forms_wrapper"}>
+                                    <form className="form_wrapper">
+                                        Добавить документ
+                                        <div>
+                                            <p>* Тип документа</p>
+                                            <select name="select" size="3" multiple className="form_input">
+                                                <option selected value="s1">Справка</option>
+                                                <option value="s2">Акт</option>
+                                                <option value="s3">Письмо</option>
+                                                <option value="s4">Выписка</option>
+                                                <option value="s5">Характеристика</option>
+                                            </select>
+                                        </div>
+                                        <div>
+                                        <p>Попытка поиска</p>
+                                            <select name="select" size="3" multiple className="form_input">
+                                                <option selected value="s1">1 (Описание)</option>
+                                                <option value="s2">2 (Описание)</option>
+                                                <option value="s3">3 (Описание)</option>
+                                                <option value="s4">4 (Описание)</option>
+                                            </select>
+                                        </div>
+
+                                        <div>
+                                            <p>Дата </p>
+                                            <input className="form_input"/>
+                                        </div>
+                                        <div>
+                                            <p>Описание </p>
+                                            <textarea className="form_input"/>
+                                        </div>
+                                        <div>
+                                        <p>Добавить автора(ов)</p>
+                                            <select name="select" size="3" multiple className="form_input">
+                                                <option  selected value="s1" > Иванов</option>
+                                                <option value="s2">Петров</option>
+                                                <option value="s3">Сидоров</option>
+                                                <option value="s4">Пирогов</option>
+                                            </select>
+                                        </div>
+                                        <div>
+                                            <p>Добавить персон, которые упоминались в документе</p>
+                                                <select name="select" size="3" multiple className="form_input">
+                                                    <option  selected value="s1" >Иванов</option>
+                                                    <option value="s2">Петров</option>
+                                                    <option value="s3">Сидоров</option>
+                                                    <option value="s4">Пирогов</option>
+                                                </select>
+                                            </div>
+                                        <button type={"submit"}>Отправить</button>
+                                    </form>
+                                    <div className="form_wrapper">
+                                        Список документов
+                                        <p>Документ</p>
+                                        <select name="select" size="3" multiple className="form_input">
+                                            <option selected value="s1">1</option>
+                                            <option value="s2">2</option>
+                                            <option value="s3">3</option>
+                                            <option value="s4">4</option>
+                                        </select>
+                                        <table>
+                                            <tr>
+                                                <td>&nbsp;</td>
+                                                <td>id документа</td>
+                                                <td>id тип документа</td>
+                                                <td>Дата</td>
+                                                <td>Описание</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Eating Habits</td>
+                                                <td>Eats everyone's leftovers</td>
+                                                <td>Nibbles at food</td>
+                                                <td>Hearty eater</td>
+                                                <td>Hearty eater</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>
             </details>
 
             <details className={"detail_wrapper"}>
                 <summary className={"summary_wrapper"}>Показание</summary>
+                                <div className={"forms_wrapper"}>
+                                    <form className="form_wrapper">
+                                        Добавить показание
+                                        <div>
+                                            <p>* Тип документа</p>
+                                            <select name="select" size="3" multiple className="form_input">
+                                                <option selected value="s1">Справка</option>
+                                                <option value="s2">Акт</option>
+                                                <option value="s3">Письмо</option>
+                                                <option value="s4">Выписка</option>
+                                                <option value="s5">Характеристика</option>
+                                            </select>
+                                        </div>
+                                        <div>
+                                        <p>Попытка поиска</p>
+                                            <select name="select" size="3" multiple className="form_input">
+                                                <option selected value="s1">1 (Описание)</option>
+                                                <option value="s2">2 (Описание)</option>
+                                                <option value="s3">3 (Описание)</option>
+                                                <option value="s4">4 (Описание)</option>
+                                            </select>
+                                        </div>
+
+                                        <div>
+                                            <p>Дата </p>
+                                            <input className="form_input"/>
+                                        </div>
+                                        <div>
+                                            <p>Описание </p>
+                                            <textarea className="form_input"/>
+                                        </div>
+                                        <div>
+                                        <p>Добавить автора(ов)</p>
+                                            <select name="select" size="3" multiple className="form_input">
+                                                <option  selected value="s1" > Иванов</option>
+                                                <option value="s2">Петров</option>
+                                                <option value="s3">Сидоров</option>
+                                                <option value="s4">Пирогов</option>
+                                            </select>
+                                        </div>
+                                        <div>
+                                            <p>Добавить персон, которые упоминались в документе</p>
+                                                <select name="select" size="3" multiple className="form_input">
+                                                    <option  selected value="s1" >Иванов</option>
+                                                    <option value="s2">Петров</option>
+                                                    <option value="s3">Сидоров</option>
+                                                    <option value="s4">Пирогов</option>
+                                                </select>
+                                            </div>
+                                        <button type={"submit"}>Отправить</button>
+                                    </form>
+                                    <div className="form_wrapper">
+                                        Список документов
+                                        <p>Документ</p>
+                                        <select name="select" size="3" multiple className="form_input">
+                                            <option selected value="s1">1</option>
+                                            <option value="s2">2</option>
+                                            <option value="s3">3</option>
+                                            <option value="s4">4</option>
+                                        </select>
+                                        <table>
+                                            <tr>
+                                                <td>&nbsp;</td>
+                                                <td>id документа</td>
+                                                <td>id тип документа</td>
+                                                <td>Дата</td>
+                                                <td>Описание</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Eating Habits</td>
+                                                <td>Eats everyone's leftovers</td>
+                                                <td>Nibbles at food</td>
+                                                <td>Hearty eater</td>
+                                                <td>Hearty eater</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>
             </details>
 
             <details className={"detail_wrapper"}>
