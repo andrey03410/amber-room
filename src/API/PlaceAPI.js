b1b1851323b2f3e0964eadedf1c7ddc19c85d50c
andrey03410 <Andrey.gel001234@mail.ru>
2023-03-01 20:34:06 +0200

    add api

---
+++ src/API/PlaceAPI.js
@@ -0,0 +1,11 @@
+export class PlaceAPI {
+    static async getPlaces() { //void
+        let response = await fetch(url);
+        if (response.ok) { // если HTTP-статус в диапазоне 200-299
+            // получаем тело ответа (см. про этот метод ниже)
+            let json = await response.json();
+        } else {
+            alert("Ошибка HTTP: " + response.status);
+        }
+    }
+}
