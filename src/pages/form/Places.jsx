ac116b4e14cb88c3f999956d0a6ec8223a29de2c
andrey03410 <Andrey.gel001234@mail.ru>
2023-03-04 14:21:11 +0200

    post places

--- src/pages/form/Places.jsx
+++ src/pages/form/Places.jsx
@@ -2,6 +2,10 @@ import React, {useEffect, useState} from 'react';
 
 const Places = () => {
     const [places, setPlaces] = useState([])
+
+    const [idPlace, setIdPlace] = useState(0)
+    const [description, setDescription] = useState("test")
+
     useEffect(() => {
         fetch("https://run.mocky.io/v3/6de17b9c-0751-418c-b695-b30fc486b6fb")
             .then(res => res.json())
@@ -9,9 +13,26 @@ const Places = () => {
                 setPlaces(result.places)
             })
     }, [])
+
+    const submit = (event) => {
+        event.preventDefault()
+        let request = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
+            body: JSON.stringify({idPlace, description})
+        }
+        fetch("test.com", request)
+            .then(response => {
+                if (response.status === 200) {
+                    alert("Место успешно добавлено")
+                } else {
+                    alert("Ошибка добавления места")
+                }
+            })
+    }
     return (
         <div className={"forms_wrapper"}>
-            <form className="form_wrapper">
+            <form className="form_wrapper" onSubmit={submit}>
                 Добавить место
                 <div>
                     <p>* Название</p>
