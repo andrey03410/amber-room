2a450b413100285d0c5e16c32e1ebffbee3ffd12
DerkachEkaterina <50298567+DerkachEkaterina@users.noreply.github.com>
2023-03-04 15:47:16 +0200

    add get version

--- src/pages/form/Versions.jsx
+++ src/pages/form/Versions.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Versions = () => {
+  const [versions, setVersion] = useState([])
+
+    const [idPlaces, setIdPlaces] = useState("test name")
+    const [description, setDescription] = useState("test desc")
+
+    useEffect(() => {
+        fetch("https://run.mocky.io/v3/45bf1284-e257-4de8-91b9-ed0e1caa8cb3")
+            .then(res => res.json())
+            .then((result) => {
+                setVersion(result.versions)
+            })
+    }, [])
     return (
         <div className={"forms_wrapper"}>
             <form className="form_wrapper">
@@ -22,20 +34,44 @@ const Versions = () => {
                                     <input className="form_input"/>
                                 </div>
                 Список версий
-                <table>
-                    <tr>
-                        <td>&nbsp;</td>
-                        <td>ID версии</td>
-                        <td>ID версии</td>
-                        <td>Описание</td>
-                    </tr>
-                    <tr>
-                        <td>Eating Habits</td>
-                        <td>Eats everyone's leftovers</td>
-                        <td>Nibbles at food</td>
-                        <td>Hearty eater</td>
-                    </tr>
-                </table>
+                <div className="scroll-table">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>id версии</th>
+                                <th>id места</th>
+                                <th>Описание</th>
+                            </tr>
+                        </thead>
+                    </table>
+                    <div className="scroll-table-body">
+                        <table>
+                            <tbody>
+                            {versions.map(item => (
+                                <tr>
+                                    <td>{item.id}</td>
+                                    <td>{item.id_places}</td>
+                                    <td>{item.description}</td>
+                                </tr>
+                            ))}
+                            {versions.map(item => (
+                                <tr>
+                                    <td>{item.id}</td>
+                                    <td>{item.id_places}</td>
+                                    <td>{item.description}</td>
+                                </tr>
+                            ))}
+                            {versions.map(item => (
+                                <tr>
+                                    <td>{item.id}</td>
+                                    <td>{item.id_places}</td>
+                                    <td>{item.description}</td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     );
