b16ffd60d24322442be4288a48c61af2ba65c129
DerkachEkaterina <50298567+DerkachEkaterina@users.noreply.github.com>
2023-03-07 21:08:30 +0200

    help

--- src/pages/form/SearchAttempts.jsx
+++ src/pages/form/SearchAttempts.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const SearchAttempts = () => {
+const [searchAttempts, setSearchAt] = useState([])
+
+    const [idVersion, setIdVersion] = useState("test name")
+    const [dateStart, setDateStart] = useState("date1")
+    const [dateFinish, setDateFinish] = useState("date2")
+
+    useEffect(() => {
+        fetch("https://run.mocky.io/v3/63200f37-abe8-487f-810f-692cc7d5197e")
+            .then(res => res.json())
+            .then((result) => {
+                setSearchAt(result.searchAttempts)
+            })
+    }, [])
     return (
-        <div>
-            <div className={"forms_wrapper"}>
+        <div className={"forms_wrapper"}>
                 <form className="form_wrapper">
                     Добавить попытку поиска
                     <div>
@@ -21,34 +33,61 @@ const SearchAttempts = () => {
                     <button type={"submit"}>Отправить</button>
                 </form>
                 <div className="form_wrapper">
-                                    Найти попытку поиска
-                                    <div>
-                                        <p>* Дата начала</p>
-                                        <input className="form_input"/>
-                                    </div>
+                    Найти попытку поиска
+                    <div>
+                        <p>* Дата начала</p>
+                        <input className="form_input"/>
+                    </div>
                     Список попыток поиска
-                    <p>* Версия</p>
 
-                    <table>
-                        <tr>
-                            <td>&nbsp;</td>
-                            <td>id попытки поиска</td>
-                            <td>id версии</td>
-                            <td>дата начала</td>
-                            <td>дата конца</td>
-                        </tr>
-                        <tr>
-                            <td>Eating Habits</td>
-                            <td>Eats everyone's leftovers</td>
-                            <td>Nibbles at food</td>
-                            <td>Hearty eater</td>
-                            <td>Hearty eater</td>
-                        </tr>
-                    </table>
+                    <div className="scroll-table">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>id попытки поиска</th>
+                                    <th>id версии</th>
+                                    <th>Дата начала</th>
+                                    <th>Дата конца</th>
+                                </tr>
+                            </thead>
+                        </table>
+                        <div className="scroll-table-body">
+                            <table>
+                                <tbody>
+                                {searchAttempts.map(item => (
+                                    <tr>
+                                        <th>id попытки поиска</th>
+                                        <th>id версии</th>
+                                        <th>Дата начала</th>
+                                        <th>Дата конца</th>
+                                    </tr>
+                                ))}
+                                {searchAttempts.map(item => (
+                                <tr>
+                                    <td>{item.id}</td>
+                                    <td>{item.id_versions}</td>
+                                    <td>{item.date_start}</td>
+                                    <td>{item.date_finish}</td>
+                                </tr>
+                                ))}
+                                {searchAttempts.map(item => (
+                                <tr>
+                                    <td>{item.id}</td>
+                                    <td>{item.id_versions}</td>
+                                    <td>{item.date_start}</td>
+                                    <td>{item.date_finish}</td>
+                                </tr>
+                                ))}
+                                </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
-
+             </div>
         </div>
+
+
+
+
     );
 };
 
