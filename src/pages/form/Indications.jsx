7e5ec5d6e3c25921ce66a1f391b498fd6b3276a9
DerkachEkaterina <50298567+DerkachEkaterina@users.noreply.github.com>
2023-03-07 23:55:24 +0200

    add get indications

--- src/pages/form/Indications.jsx
+++ src/pages/form/Indications.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Indications = () => {
+    const [indications, setIndications] = useState([])
+
+    const [idPersons, setIdPersons] = useState("test name")
+    const [testimony, setTestimony] = useState("test desc")
+    const [idVersions, setIdVersions] = useState("test desc")
+    const [date, setDate] = useState("test date")
+
+    useEffect(() => {
+        fetch("https://run.mocky.io/v3/10ce9c86-ab08-416a-aafc-4d04a143a995")
+            .then(res => res.json())
+            .then((result) => {
+                setIndications(result.indications)
+            })
+    }, [])
     return (
         <div className={"forms_wrapper"}>
             <form className="form_wrapper">
@@ -50,29 +64,52 @@ const Indications = () => {
                             <input className="form_input"/>
                         </div>
                 Список показаний
-                <p>Показание</p>
-                <select name="select" size="3" multiple className="form_input">
-                    <option selected value="s1">1</option>
-                    <option value="s2">2</option>
-                    <option value="s3">3</option>
-                    <option value="s4">4</option>
-                </select>
-                <table>
-                    <tr>
-                        <td>&nbsp;</td>
-                        <td>id показания</td>
-                        <td>id автора</td>
-                        <td>Показание</td>
-                        <td>Дата</td>
-                    </tr>
-                    <tr>
-                        <td>Eating Habits</td>
-                        <td>Eats everyone's leftovers</td>
-                        <td>Nibbles at food</td>
-                        <td>Hearty eater</td>
-                        <td>Hearty eater</td>
-                    </tr>
-                </table>
+                <div className="scroll-table">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>id показания</th>
+                                <th>id персоны</th>
+                                <th>Показание</th>
+                                <th>id версии</th>
+                                <th>Дата</th>
+                            </tr>
+                        </thead>
+                    </table>
+                    <div className="scroll-table-body">
+                        <table>
+                            <tbody>
+                            {indications.map(item => (
+                                <tr>
+                                    <td>{item.id}</td>
+                                    <td>{item.id_persons}</td>
+                                    <td>{item.testimony}</td>
+                                    <td>{item.id_versions}</td>
+                                    <td>{item.date}</td>
+                                </tr>
+                            ))}
+                            {indications.map(item => (
+                                <tr>
+                                    <td>{item.id}</td>
+                                    <td>{item.id_persons}</td>
+                                    <td>{item.testimony}</td>
+                                    <td>{item.id_versions}</td>
+                                    <td>{item.date}</td>
+                                </tr>
+                            ))}
+                            {indications.map(item => (
+                                <tr>
+                                    <td>{item.id}</td>
+                                    <td>{item.id_persons}</td>
+                                    <td>{item.testimony}</td>
+                                    <td>{item.id_versions}</td>
+                                    <td>{item.date}</td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     );
