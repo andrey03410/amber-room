0cc456d9f2aadb5c46f69b5e77bafd7c494eb8a0
andrey03410 <Andrey.gel001234@mail.ru>
2023-04-24 22:46:54 +0200

    lib

--- src/pages/MapView.jsx
+++ src/pages/MapView.jsx
@@ -1,11 +1,12 @@
 import React, { useRef, useEffect, useState } from 'react';
-import Map from 'react-map-gl';
 
 
 
 const MapView = () => {
     return (
-        <div>Карта</div>
+        <div>
+            Карта
+        </div>
     );
 };
 
