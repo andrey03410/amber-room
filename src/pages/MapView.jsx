365d1d8f716a8c6568bc13688b147cf0b34ce8dc
DerkachEkaterina <50298567+DerkachEkaterina@users.noreply.github.com>
2023-04-24 22:55:18 +0200

    add map

--- src/pages/MapView.jsx
+++ src/pages/MapView.jsx
@@ -1,13 +1,19 @@
 import React, { useRef, useEffect, useState } from 'react';
-
+import { MapContainer, CircleMarker, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 
 const MapView = () => {
     return (
         <div>
-            Карта
-        </div>
+            <MapContainer
+              style={{ height: "480px", width: "100%" }}
+              zoom={1}
+              center={[-0.09, 51.505]}>
+            </MapContainer>
+          </div>
     );
 };
 
 export default MapView;
+
