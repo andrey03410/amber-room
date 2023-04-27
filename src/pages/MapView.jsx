dc117fdab801cc2d2f9fc0daa4e99e6975eefbaf
andrey03410 <Andrey.gel001234@mail.ru>
2023-04-27 13:02:29 +0200

    update lib yandex map

--- src/pages/MapView.jsx
+++ src/pages/MapView.jsx
@@ -1,15 +1,21 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { YMaps, Map } from 'react-yandex-maps';
+import React, {useRef, useEffect, useState, useMemo} from 'react';
+import {YMaps, Map, TypeSelector, useYMaps} from '@pbe/react-yandex-maps';
+
 
 const MapView = () => {
+    const mapRef = useRef(null);
+
     return (
         <YMaps>
             <div>
-                <Map width={1280} height={720} defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
+                <Map width={1280} height={720} defaultState={{center: [55.75, 37.57], zoom: 9}}>
+                    <TypeSelector mapTypes={['yandex#map']} options={{float: "right"}}/>
+                </Map>
             </div>
         </YMaps>
     );
 };
 
 export default MapView;
+
 
