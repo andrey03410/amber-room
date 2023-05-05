54f9432776de79c7d20e12ee0a4126076307c84f
andrey03410 <Andrey.gel001234@mail.ru>
2023-05-05 18:53:01 +0200

    add historical map

--- src/pages/MapView.jsx
+++ src/pages/MapView.jsx
@@ -1,14 +1,22 @@
-import React, {useRef, useEffect, useState, useMemo} from 'react';
-import {YMaps, Map, TypeSelector, useYMaps} from '@pbe/react-yandex-maps';
-import MapChildren from "./MapChildren";
+import React, { useRef, useEffect, useState } from 'react';
+import { MapContainer, CircleMarker, TileLayer, Popup, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
+const position = [54.721, 20.586]
 
 const MapView = () => {
     return (
-        <YMaps>
-            <MapChildren/>
-        </YMaps>
-    );
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: "1000px", width: "100%" }}>
+            <TileLayer
+                url="https://api.maptiler.com/tiles/8a9d5d1c-61e5-41ef-b9a2-58a2eb591d30/{z}/{x}/{y}.jpg?key=PkzZI0EXNxvEXbU8mpRh"
+            />
+            <Marker position={position}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
+    )
 };
 
 export default MapView;
