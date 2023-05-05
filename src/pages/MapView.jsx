c1206b230eafe1d4eb6d07852c2a3bb5908aad45
andrey03410 <Andrey.gel001234@mail.ru>
2023-05-06 00:01:56 +0200

    add zoom limit

--- src/pages/MapView.jsx
+++ src/pages/MapView.jsx
@@ -6,7 +6,7 @@ const position = [54.721, 20.586]
 
 const MapView = () => {
     return (
-        <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: "1000px", width: "100%" }}>
+        <MapContainer center={position} zoom={13} style={{ height: "1000px", width: "100%" }} maxZoom={14} minZoom={10}>
             <TileLayer
                 url="https://api.maptiler.com/tiles/8a9d5d1c-61e5-41ef-b9a2-58a2eb591d30/{z}/{x}/{y}.jpg?key=PkzZI0EXNxvEXbU8mpRh"
             />
