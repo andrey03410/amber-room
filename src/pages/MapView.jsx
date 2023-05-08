e346126fe8d64403cdbc70f2fce2c82084a86ccf
andrey03410 <Andrey.gel001234@mail.ru>
2023-05-08 15:37:24 +0200

    remove unused deps and make map fullscreen

--- src/pages/MapView.jsx
+++ src/pages/MapView.jsx
@@ -17,7 +17,7 @@ const MapView = () => {
     const [places, setPlaces] = useState([{id: 1, name: "Королевский замок", description: "Тестовое описание1", coord_x: 54.721, coord_y: 20.586},
         {id: 2, name: "Не Королевский замок", description: "Тестовое описание2", coord_x: 54.721, coord_y: 20.686}])
     return (
-        <MapContainer center={position1} zoom={13} style={{ height: "1000px", width: "100%" }} maxZoom={14} minZoom={12}>
+        <MapContainer center={position1} zoom={13} style={{ height: "calc(100vh - 60px)", width: "100%" }} maxZoom={14} minZoom={12}>
             <TileLayer
                 url="https://api.maptiler.com/tiles/8a9d5d1c-61e5-41ef-b9a2-58a2eb591d30/{z}/{x}/{y}.jpg?key=PkzZI0EXNxvEXbU8mpRh"
             />
