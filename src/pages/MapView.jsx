cf9a3a77b15b12df034529202e1bd5c2a0da46f8
DerkachEkaterina <50298567+DerkachEkaterina@users.noreply.github.com>
2023-05-10 21:06:22 +0200

    add markers

--- src/pages/MapView.jsx
+++ src/pages/MapView.jsx
@@ -8,22 +8,37 @@ const position2 = [54.721, 20.686]
 
 function GetIcon(_iconSize) {
     return L.icon({
-        iconUrl: require("./map_marker.png"),
+        iconUrl: require("./marker.png"),
         iconSize: [_iconSize]
     })
 }
 
 const MapView = () => {
-    const [places, setPlaces] = useState([{id: 1, name: "Королевский замок", description: "Тестовое описание1", coord_x: 54.721, coord_y: 20.586},
-        {id: 2, name: "Не Королевский замок", description: "Тестовое описание2", coord_x: 54.721, coord_y: 20.686}])
+    const [places, setPlaces] = useState([])
+
+    const [selectLoading, setSelectLoading] = useState(true)
+
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:5000/getPlaces")
+            .then(res => res.json())
+            .then((result) => {
+                setPlaces(result.places)
+
+
+                setSelectLoading(false)
+            })
+    }, [])
+
+
     return (
         <MapContainer center={position1} zoom={13} style={{ height: "calc(100vh - 60px)", width: "100%" }} maxZoom={14} minZoom={12}>
-            <TileLayer
-                url="https://api.maptiler.com/tiles/8a9d5d1c-61e5-41ef-b9a2-58a2eb591d30/{z}/{x}/{y}.jpg?key=PkzZI0EXNxvEXbU8mpRh"
-            />
+        <TileLayer url="https://api.maptiler.com/tiles/8a9d5d1c-61e5-41ef-b9a2-58a2eb591d30/{z}/{x}/{y}.jpg?key=PkzZI0EXNxvEXbU8mpRh"/>
+
+
             {places.map((item) => {
                 return(
-                    <Marker position={[item.coord_x, item.coord_y]} icon={GetIcon(30)}>
+                    <Marker position={[item.coord_x, item.coord_y]} icon={GetIcon(60)}>
                         <Popup>
                             Название: {item.name} <br />
                             Описание: {item.description}
@@ -31,6 +46,8 @@ const MapView = () => {
                     </Marker>
                 )
             })}
+
+
         </MapContainer>
     )
 };
