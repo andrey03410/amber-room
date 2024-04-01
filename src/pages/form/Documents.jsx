ac4da8af19cbbcf794d423a6bc3df49b9cbfef46
andrey03410 <90139164+andrey03410@users.noreply.github.com>
2024-04-01 20:35:59 +0200

    add support file uploading!

--- src/pages/form/Documents.jsx
+++ src/pages/form/Documents.jsx
@@ -24,8 +24,10 @@ const Documents = (props) => {
     const [selectLoadingPers, setSelectLoadingPers] = useState(true)
     const [selectLoadingSearchAtt, setSelectLoadingSearchAtt] = useState(true)
 
-     const [personsSelectValue, setPersonsSelectValue] = useState({id_persons: ""})
-     const [authorsSelectValue, setAuthorsSelectValue] = useState({id_authors: ""})
+    const [personsSelectValue, setPersonsSelectValue] = useState({id_persons: ""})
+    const [authorsSelectValue, setAuthorsSelectValue] = useState({id_authors: ""})
+
+    const [file, setFile] = useState(null);
 
 
     useEffect(() => {
@@ -70,7 +72,6 @@ const Documents = (props) => {
     }, [])
 
 
-
     const searchDoc = (id_person) => {
         let request = {
             method: 'POST',
@@ -95,17 +96,26 @@ const Documents = (props) => {
     const submit = (event) => {
         event.preventDefault()
         let id_person = []
-                personsSelectValue.map((item) => {
-                    id_person.push(item.value)
-                })
+        personsSelectValue.map((item) => {
+            id_person.push(item.value)
+        })
         let id_author = []
-                        authorsSelectValue.map((item) => {
-                            id_author.push(item.value)
-                        })
+        authorsSelectValue.map((item) => {
+            id_author.push(item.value)
+        })
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('data', JSON.stringify({
+            id_type_doc,
+            id_search_attempts,
+            date,
+            description,
+            id_author,
+            id_person,
+        }));
         let request = {
             method: 'POST',
-            headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
-            body: JSON.stringify({id_type_doc, id_search_attempts, date, description, id_author, id_person, imageDesc, images})
+            body: formData
         }
         fetch("http://127.0.0.1:5000/addDocument", request)
             .then(response => {
@@ -116,33 +126,11 @@ const Documents = (props) => {
                 }
             })
     }
-    const addButtonImage = (event) => {
-        event.preventDefault()
-        setImageDesc(prevState => {
-            return [...prevState, "123"]
-        })
-        setImages(prevState => {
-            return [...prevState, null]
-        })
+    const handleFileUpload = async (e) => {
+        const file = e.target.files[0];
+        setFile(file);
     }
 
-    const delButtonImage = (event) => {
-        event.preventDefault()
-        setImageDesc(prevState => {
-            let array = [...prevState]
-            if (array.length > 1) {
-                array.pop()
-            }
-            return array
-        })
-        setImages(prevState => {
-            let array = [...prevState]
-            if (array.length > 1) {
-                array.pop()
-            }
-            return array
-        })
-    }
     return (
         <div className={"forms_wrapper"}>
             <form className="form_wrapper" onSubmit={submit}>
@@ -188,49 +176,15 @@ const Documents = (props) => {
                     <p className={"title_field"}>Добавить персон, которые упоминались в документе</p>
                     <Select options={personList} isLoading={selectLoadingPers} isMulti placeholder={"Выберите персон"}
                             onChange={(newValue) => {
-                            setPersonsSelectValue(newValue)
+                                setPersonsSelectValue(newValue)
 
                             }}/>
                 </div>
                 <div>
-                    <p className={"title_field"}>Добавить фото </p>
-                    <div className="forms_wrapper">
-                        <div>
-                            <p className={"title_field"}>Фото </p>
-                        </div>
-                        <div>
-                            <p className={"title_field"}>Описание </p>
-                        </div>
+                    <p className={"title_field"}>Добавить исходный файл</p>
+                    <div>
+                        <input type={"file"} onChange={handleFileUpload}/>
                     </div>
-
-                    {imageDesc.map((item, index) => (
-                        <div className="forms_wrapper">
-                            <div>
-                                <input type={"file"} onChange={(event) => {
-                                    setImages(prevState => {
-                                        let array = [...prevState]
-                                        array[index] = event.target.files[0]
-                                        console.log(array)
-                                        return array
-                                    })
-
-                                }}/>
-                            </div>
-                            <div>
-                                <input className="form_input" onChange={(event => {
-                                    setImageDesc(prevState => {
-                                        let array = [...prevState]
-                                        array[index] = event.target.value
-                                        return array
-                                    })
-                                })}/>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-                <div>
-                    <button onClick={addButtonImage}>Добавить ячейку</button>
-                    <button onClick={delButtonImage}>Удалить ячейку</button>
                 </div>
                 <button className={"submit_button"}>Отправить</button>
             </form>
@@ -264,29 +218,24 @@ const Documents = (props) => {
                                     <tr>
                                         <td>{item.id}</td>
                                         {typeDocList.map(nat => {
-                                                if (nat.value === item.id_type_doc) {
-                                                    return (
-                                                        <td>{nat.label}</td>
-                                                    )
-                                                }
-                                            })}
+                                            if (nat.value === item.id_type_doc) {
+                                                return (
+                                                    <td>{nat.label}</td>
+                                                )
+                                            }
+                                        })}
                                         {searchAttList.map(nat => {
-                                                if (nat.value == item.id_search_attempts) {
-                                                    return (
-                                                        <td>{nat.label}</td>
-                                                    )
-                                                }
-                                            })}
-
-
-
+                                            if (nat.value == item.id_search_attempts) {
+                                                return (
+                                                    <td>{nat.label}</td>
+                                                )
+                                            }
+                                        })}
                                         <td>{item.date}</td>
                                         <td>{item.description}</td>
                                     </tr>
                                 )
-
                             })}
-
                             </tbody>
                         </table>
                     </div>
